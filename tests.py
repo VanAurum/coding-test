a015a484552412421d089c380f7e00b6126b5c1e
Kevin Vecmanis <vanaurum@vanurum-master.hitronhub.home>
2018-10-12 09:24:04 -0700

    initial commit

---
+++ tests.py
@@ -0,0 +1,19 @@
+#tests.py
+
+'''About tests.py:
+This module contains all the unit tests for testing both the main solution and the helper functions in solution/helpers.py
+'''
+
+class TestSolution(unittest.TestCase):
+    
+    def test_time_range_is_integer_tuple(self):
+        self.assertEqual(convert_time_range_to_integers('9:00-10:00'), (540,600))
+        
+    def test_clean_time_range_string_removes_whitespace(self):
+        self.assertEqual(clean_time_range_string('9:00  -  10:00pm '), '9:00-10:00')
+    
+
+if '__name__'=='__main__':
+    
+suite=unittest.TestLoader().loadTestsFromTestCase(TestSolution)
+unittest.TextTestRunner(verbosity=2).run(suite) 
